a4e7c6fbbcd9910275d4b5de5dec9e45d9c86d93
desang12 <1031khw@gmail.com>
2016-11-02 02:36:02 +0900

    init

--- test/main.c
+++ test/main.c
@@ -1,15 +1,9 @@
-//
-//  main.c
-//  test
-//
-//  Created by Hyunwoo Kim on 2016. 11. 2..
-//  Copyright © 2016년 Hyunwoo Kim. All rights reserved.
-//
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void) {
+    char name[32] = {0};
+    fgets(name, sizeof(name), stdin);
+    printf("%s\n", name);
     return 0;
 }
